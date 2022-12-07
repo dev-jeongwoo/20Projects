341b69a0d54ec4526d1e9a2efffdfe9bd8600f31
dev-jeongwoo <76766017+dev-jeongwoo@users.noreply.github.com>
2022-12-07 18:02:25 +0900

    inf-scroll setup

--- quote-generator/src/app.ts
+++ quote-generator/src/app.ts
@@ -1,1 +1,57 @@
 import './style.css';
+
+const quoteContainer = document.getElementById(
+  'quote-container',
+) as HTMLDivElement;
+const quoteText = document.getElementById('quote') as HTMLSpanElement;
+const quoteAuthor = document.getElementById('author') as HTMLSpanElement;
+const loading = document.getElementById('loading') as HTMLDivElement;
+
+const btnTwitter = document.getElementById('twitter') as HTMLButtonElement;
+const btnNewquote = document.getElementById('new-quote') as HTMLButtonElement;
+
+interface Quote {
+  author: string;
+  tag: string;
+  text: string;
+}
+
+function showLoadingRipple() {
+  quoteContainer.hidden = true;
+  loading.hidden = false;
+}
+
+function hideLoadingRipple() {
+  quoteContainer.hidden = false;
+  loading.hidden = true;
+}
+
+function printRandomQuote(quote: Quote) {
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = quote.text;
+  quoteAuthor.textContent = quote.author;
+}
+
+function randomQuote(quotes: Quote[]) {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+async function getQuotes() {
+  showLoadingRipple();
+  try {
+    const apiUrl =
+      'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+    const response = await fetch(apiUrl);
+    const quotes = (await response.json()) as Quote[];
+    const quote = randomQuote(quotes);
+    printRandomQuote(quote);
+    console.log('quote', quote);
+  } catch (error) {
+    console.error(error);
+  }
+}
+getQuotes();
